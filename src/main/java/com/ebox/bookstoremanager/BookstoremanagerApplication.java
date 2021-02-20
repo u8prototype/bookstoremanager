370316ebf1c38615099f5e04010c9625479743a5
@Stephanie2020 <davila.stephanie@outlook.com>
2021-02-20 12:23:29 -0300

    Create .travis.yml file

--- src/main/java/com/ebox/bookstoremanager/BookstoremanagerApplication.java
+++ src/main/java/com/ebox/bookstoremanager/BookstoremanagerApplication.java
@@ -2,8 +2,14 @@ package com.ebox.bookstoremanager;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
+@EnableSwagger2
 public class BookstoremanagerApplication {
 
 	public static void main(String[] args) {
