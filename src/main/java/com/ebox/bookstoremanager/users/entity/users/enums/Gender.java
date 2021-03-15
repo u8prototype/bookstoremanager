d6a0cac9d1a844d184272c08b5c95e93eb847a05
@Stephanie2020 <davila.stephanie@outlook.com>
2021-03-15 18:25:27 -0300

    update 5

--- src/main/java/com/ebox/bookstoremanager/users/entity/users/enums/Gender.java
+++ src/main/java/com/ebox/bookstoremanager/users/entity/users/enums/Gender.java
@@ -9,14 +9,8 @@ import lombok.Getter;
 @AllArgsConstructor
 public enum Gender {
 
-	MALE("Male"),
-	FEMALE("Female");
-		
-	private String description;
-	
-	Gender (String description){		
-		this.description = description;		
-	}	
+	MALE,
+	FEMALE;
 	
 	
 	
